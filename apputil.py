7a2c811619245c6e7e35cdf9a0173f816f399098
ryansmith04 <rjs14@iu.edu>
2026-09-14 20:27:11 -0400

    implement ways, lowest_score, and sort_names functions in apputil.py

--- apputil.py
+++ apputil.py
@@ -4,10 +4,20 @@ import numpy as np
 # update/add code below ...
 
 def ways(n):
-    return None
+    count = 0
+
+    for nickels in range(n // 5 + 1):
+        pennies = n - (nickels * 5)
+
+        if pennies >= 0:
+            count += 1
+
+    return count
 
 def lowest_score(names, scores):
-    return None
+    index = np.argmin(scores)
+    return names[index]
 
 def sort_names(names, scores):
-    return None
+    index = np.argsort(scores)[::-1]
+    return names[index]
