b110c0393b0e8c6ffaa002a107499051f2b091d7
ryansmith04 <rjs14@iu.edu>
2026-09-15 17:57:19 -0400

    Add docstrings to ways, lowest_score, and sort_names functions for clarity

--- apputil.py
+++ apputil.py
@@ -4,17 +4,31 @@ import numpy as np
 # update/add code below ...
 
 def ways(n):
+    """Return the number of ways to make change for n cents using only nickels and pennies."""
     count = 0
+
+
     for nickels in range(n // 5 + 1):
         pennies = n - (nickels * 5)
-        if pennies >= 0:
+
+
+         if pennies >= 0:
             count += 1
+
+
     return count
 
+
 def lowest_score(names, scores):
+    """Return the name of the student with the lowest score."""
     index = np.argmin(scores)
+
+
     return names[index]
 
 def sort_names(names, scores):
+    """Return the list of names sorted by their corresponding scores in descending order."""
     index = np.argsort(scores)[::-1]
+
+
     return np.array(names)[index]
